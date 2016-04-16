db5092c64ce55f272a02fe6d520957b949029dae
Oribe Abad <subcero000@gmail.com>
2016-04-16 13:47:15 -0500

    adding Rotacion

--- Tanke.java
+++ Tanke.java
@@ -2,17 +2,22 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class Tanke extends JFrame{
+  AffineTransform identidad;
   private JPanel contentPane;
   private int movx=0;
   private int movy=0;
+  private int disx=0;
+  private int disy=0;
   private Tanque player;
   private int direccion=0;
   public int getX(){
@@ -80,28 +85,37 @@ public class Tanke extends JFrame{
           vx1[0]=100;vx1[1]=50;vx1[2]=50;vx1[3]=150;vx1[4]=150;
           int [] vy1 = new int[5];
           vy1[0]=50;vy1[1]=100;vy1[2]=150;vy1[3]=150;vy1[4]=100;
-          player=new Tanque(vx1,vy1,5,0);
+          player=new Tanque(vx1,vy1,5,0,vx1[0],vy1[1]);
+          identidad = new AffineTransform();
 
       }
       public void paint (Graphics g){
-          super.paint(g);
+          Graphics2D g2 = (Graphics2D) g;
+          super.paint(g2);
 
-          g.setColor (Color.blue);
-
+          g2.setColor (Color.blue);
+          g2.setTransform(identidad);
           //g.drawLine (0, 70, 100, 70);
           //g.fillRoundRect(150, 70, 50, 70);
           //g.drawRoundRect (250, 70, 50, 70, 6, 6);
           //g.drawOval (350, 70, 50, 70);
           player.sumarApuntosX(movx);
           player.sumarApuntosY(movy);
-          if(player.getDisparar()==true){
+          if(player.getDisparar()==true && player.getElementY()+disy>-1){
             System.out.println("disparo");
-            
+
+            g2.fillOval(player.getElementX(),player.getElementY()+disy,20,20);
+            disy-=1;
           }
-          player.setDisparar(false);
+          if(player.getElementY()+disy<0){
+              disy=0;
+              player.setDisparar(false);
+          }
+          //player.setDisparar(false);
           movx=0;
           movy=0;
-          g.drawPolygon (player.getPuntosX(),player.getPuntosY(),player.getNL());
+          //g2.rotate(Math.toRadians(90.0));
+          g2.fillPolygon (player.getPuntosX(),player.getPuntosY(),player.getNL());
 
           //g.setColor (Color.red);
           //g.fillRect (150, 270, 50, 70);
--- Tanque.java
+++ Tanque.java
@@ -4,12 +4,105 @@ class Tanque{
   private int nl;
   private boolean disparo;
   private int indxp;
-  Tanque(int []arre1, int []arre2,int n,int in){
+  private int prx;
+  private int pry;
+  private int dir;
+  Tanque(int []arre1, int []arre2,int n,int in,int puntox,int puntoy){
     puntosx=arre1;
     puntosy=arre2;
     nl=n;
     disparo=false;
     indxp=in;
+    prx=puntox;
+    pry=puntoy;
+    dir=90;
+  }
+  public int getDir(){
+    return dir;
+  }
+  public void derecha(){
+    if(dir==90){
+      this.rotar90();
+      this.rotar90();
+      this.rotar90();
+    }
+    if(dir==180){
+      this.rotar90();
+      this.rotar90();
+    }
+    if(dir==270){
+      this.rotar90();
+    }
+    setProtacion(puntosx[1],Math.abs((puntosy[4]-puntosy[1])));
+    dir=360;
+  }
+  public void izquierda(){
+    if(dir==90){
+      this.rotar90();
+    }
+    if(dir==270){
+      this.rotar90();
+      this.rotar90();
+      this.rotar90();
+    }
+    if(dir==360){
+      this.rotar90();
+      this.rotar90();
+    }
+    setProtacion(puntosx[1],Math.abs((puntosy[4]-puntosy[1])));
+    dir=180;
+  }
+  public void izquierda(){
+    if(dir==90){//componer no esta terminado
+      this.rotar90();
+    }
+    if(dir==270){
+      this.rotar90();
+      this.rotar90();
+      this.rotar90();
+    }
+    if(dir==360){
+      this.rotar90();
+      this.rotar90();
+    }
+    setProtacion(puntosx[1],Math.abs((puntosy[4]-puntosy[1])));
+    dir=180;
+  }
+
+  public void rotar90(){
+    for (int i=0;i<nl;i++) {
+      int[] ps=getNewP(puntosx[i]-prx,puntosy[i]-pry);
+      puntosx[i]=ps[0]+prx;
+      puntosy[i]=ps[1]+pry;
+    }
+
+  }
+
+  public void setProtacion(int p,int p2){
+    prx=p;
+    pry=p2;
+  }
+
+  public int[] getNewP(int x,int y){
+    int []ps=new int[2];
+    ps[0] = -y;
+    ps[1]= x;
+    return ps;
+  }
+
+  public int  getElementX(){
+    if(indxp<puntosx.length){
+            return puntosx[indxp];
+    }
+    return -1;
+
+  }
+  public int  getElementY(){
+    if(indxp<puntosy.length){
+       return puntosy[indxp];
+    }
+    return -1;
+
   }
 
   public int getPunta(){
@@ -45,20 +138,34 @@ class Tanque{
   public int getNL(){
     return nl;
   }
+  public void print(){
+    for (int i=0;i<nl;i++) {
+      System.out.println(+puntosx[i]+","+puntosy[i]);
+    }
+  }
 
 
   public static void main(String[] args) {
-    int [][] ar=new int[2][5];
-    ar[0][0]=120;
-    ar[1][0]=120;
-    ar[0][1]=120;
-    ar[1][1]=170;
-    ar[0][2]=170;
-    ar[1][2]=170;
-    ar[0][3]=170;
-    ar[1][3]=120;
-    ar[0][4]=145;
-    ar[1][4]=80;
+    int x=0;
+    int y=0;
+    int [] vx1 = new int[5];
+    vx1[0]=100+x;vx1[1]=50+x;vx1[2]=50+x;vx1[3]=150+x;vx1[4]=150+x;
+    int [] vy1 = new int[5];
+    vy1[0]=50+y;vy1[1]=100+y;vy1[2]=150+y;vy1[3]=150+y;vy1[4]=100+y;
+    Tanque player=new Tanque(vx1,vy1,5,0,vx1[0],vy1[1]);
+    player.print();
+    player.rotar90();
+    System.out.println("________");
+    player.print();
+    player.rotar90();
+    System.out.println("________");
+    player.print();
+    player.rotar90();
+    System.out.println("________");
+    player.print();
+
+
+
 
   }
 
